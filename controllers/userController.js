8ac0cb514e1aa267e4e03b8fdff45ea62bcfdbf5
OshithaDilshan <osithadil55@gmail.com>
2025-07-21 16:10:11 +0530

    Day 20 done 👍

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,11 +2,12 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
-
+import axios from "axios";
+import nodemailer from "nodemailer";
+import OTP from "../models/otp.js";
 dotenv.config();
 
 export function createUser(req, res) {
-
     if (req.body.role == "admin") {
         if (req.user != null) {
             if (req.user.role != "admin") {
@@ -32,6 +33,7 @@ export function createUser(req, res) {
         password: hashedPassword,
         role: req.body.role,
     })
+
 
     user.save().then(
         () => {
@@ -69,22 +71,212 @@ export function loginUser(req, res) {
                             role: user.role,
                             img: user.img
                         },
-                        process.env.JWT_KEY//password ekak 
+                        process.env.JWT_KEY
                     )
+
 
                     res.json({
                         message: "Login successful",
                         token: token,
                         role: user.role
                     })
+
                 } else {
                     res.status(401).json({
                         message: "Invalid password"
                     })
                 }
             }
+
         }
     )
+
+}
+
+export async function loginWithGoogle(req, res) {
+    const token = req.body.accessToken;
+    if (token == null) {
+        res.status(400).json({
+            message: "Access token is required"
+        });
+        return;
+    }
+    const response = await axios.get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    console.log(response.data);
+
+    const user = await User.findOne({
+        email: response.data.email
+    })
+
+    if (user == null) {
+        const newUser = new User(
+            {
+                email: response.data.email,
+                firstName: response.data.given_name,
+                lastName: response.data.family_name,
+                password: "googleUser",
+                img: response.data.picture
+            }
+        )
+        await newUser.save();
+        const token = jwt.sign(
+            {
+                email: newUser.email,
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                role: newUser.role,
+                img: newUser.img
+            },
+            process.env.JWT_KEY
+        )
+        res.json({
+            message: "Login successful",
+            token: token,
+            role: newUser.role
+        })
+
+    } else {
+
+        const token = jwt.sign(
+            {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                role: user.role,
+                img: user.img
+            },
+            process.env.JWT_KEY
+        )
+        res.json({
+            message: "Login successful",
+            token: token,
+            role: user.role
+        })
+
+    }
+
+}
+const transport = nodemailer.createTransport({
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.APP_USER_EMAIL,
+        pass: process.env.APP_USER_EMAIL_PASSWORD
+    }
+})
+export async function sendOTP(req, res) {
+    //javy zfzy dwsd rmbg
+    const randomOTP = Math.floor(100000 + Math.random() * 900000);
+    const email = req.body.email;
+    if (email == null) {
+        res.status(400).json({
+            message: "Email is required"
+        });
+        return;
+
+    }
+    const user = await User.findOne({
+        email: email
+    })
+    if (user == null) {
+        res.status(404).json({
+            message: "User not found"
+        })
+    }
+
+    //delete all otps
+    await OTP.deleteMany({
+        email: email
+    })
+
+
+    const message = {
+        from: process.env.APP_USER_EMAIL,
+        to: email,
+        subject: "Resetting password for crystal beauty clear.",
+        text: "This your password reset OTP : " + randomOTP
+    }
+
+    const otp = new OTP({
+        email: email,
+        otp: randomOTP
+    })
+    await otp.save()
+    transport.sendMail(message, (error, info) => {
+        if (error) {
+            res.status(500).json({
+                message: "Failed to send OTP",
+                error: error
+            });
+        } else {
+            res.json({
+                message: "OTP sent successfully",
+                otp: randomOTP
+            });
+        }
+    }
+    )
+}
+
+export async function resetPassword(req, res) {
+    const otp = req.body.otp
+    const email = req.body.email
+    const newPassword = req.body.newPassword
+    console.log(otp)
+    const response = await OTP.findOne({
+        email: email
+    })
+
+    if (response == null) {
+        res.status(500).json({
+            message: "No otp requests found please try again"
+        })
+        return
+    }
+    if (otp == response.otp) {
+        await OTP.deleteMany(
+            {
+                email: email
+            }
+        )
+        console.log(newPassword)
+
+        const hashedPassword = bcrypt.hashSync(newPassword, 10)
+        const response2 = await User.updateOne(
+            { email: email },
+            {
+                password: hashedPassword
+            }
+        )
+        res.json({
+            message: "password has been reset successfully"
+        })
+    } else {
+        res.status(403).json({
+            meassage: "OTPs are not matching!"
+        })
+    }
+
+}
+
+export function getUser(req, res) {
+    if (req.user == null) {
+        res.status(403).json({
+            message: "You are not authorized to view user details"
+        })
+        return
+    } else {
+        res.json({
+            ...req.user
+        })
+    }
 }
 
 export function isAdmin(req) {
