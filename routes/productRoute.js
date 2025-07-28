5eb3c999c5c0ebce98838d894d70e56d53679d4a
OshithaDilshan <osithadil55@gmail.com>
2025-07-28 09:42:26 +0530

    Day 21 ✏

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { deleteProduct, getProductById, getProducts, saveProduct, updateProduct } from "../controllers/productController.js";
+import { deleteProduct, getProductById, getProducts, saveProduct, searchProducts, updateProduct } from "../controllers/productController.js";
 
 const productRouter = express.Router();
 
@@ -8,6 +8,8 @@ productRouter.post("/", saveProduct);
 productRouter.delete("/:productId", deleteProduct);
 productRouter.put("/:productId", updateProduct);
 productRouter.get("/:productId", getProductById);
+productRouter.get("/search/:query", searchProducts);
+
 
 export default productRouter;
 
